a956942379621c7b7dde242798a7bbd85089756b
EugeneW910428 <149223513+EugeneW910428@users.noreply.github.com>
2024-05-23 21:59:49 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
 import marathonLogo from '../assets/marathon.jpeg';
-
+import './App.css'; // Import the main CSS file
 
 const Navbar = () => {
   return (
-    <Menu mode="horizontal" theme="dark">
-      <Menu.Item key="logo" disabled>
-        <img src={marathonLogo} className="logo" alt="Marathon Logo" />
+    <Menu mode="horizontal" theme="dark" className="navbar-menu">
+      <Menu.Item key="logo" className="logo-item" disabled>
+        <Link to="/">
+          <img src={marathonLogo} className="logo" alt="Marathon Logo" />
+        </Link>
       </Menu.Item>
       <Menu.Item key="donation">
         <Link to="/donation">Donation</Link>
@@ -36,4 +38,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
